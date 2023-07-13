2b3d7b09feb11b2c71140a7c2ad72d41186e938b
kenan <kennyolmez@gmail.com>
2023-07-13 12:25:58 +0200

    generic for less repetition and more focused ConversationClient methods

--- TwilioConversations/ConversationCLient.cs
+++ TwilioConversations/ConversationCLient.cs
@@ -20,36 +20,34 @@ public class ConversationClient
     {
         var content = new FormUrlEncodedContent(new Dictionary<string, string> { { "FriendlyName", friendlyName ?? string.Empty } });
 
-        var response = await _client.PostHttpRequest(content);
+        var response = await _client.MakeHttpRequest<ConversationResource>(HttpMethod.Post, content);
 
-        var deserialized = JsonSerializer.Deserialize<ConversationResource>(response);
-
-        return deserialized?.Sid ?? throw new InvalidOperationException("Failed to deserialize response.");
+        return response.Sid ?? throw new InvalidOperationException("Failed to deserialize response.");
     }
 
   
     public async Task<IEnumerable<MessageResource>> GetConversation(string conversationSid, int pageSize = 25, string? pageToken = null)
     {
-        var response = await _client.GetHttpRequest(uri: UrlHelper.GetConversationsUri(conversationSid, pageSize, pageToken));
+        var response = await _client.MakeHttpRequest<ConversationResource>(HttpMethod.Get, uri: UrlHelper.GetConversationsUri(conversationSid, pageSize, pageToken));
 
-        var deserialized = JsonSerializer.Deserialize<ConversationResource>(response);
-
-        return deserialized?.Messages ?? Enumerable.Empty<MessageResource>();
+        return response.Messages;
     }
 
     public async Task AddParticipants(string conversationSid, string identity)
     {
         var content = new FormUrlEncodedContent(new Dictionary<string, string> { { "Identity", identity} });
 
-        await _client.PostHttpRequest(content: content, uri: UrlHelper.AddParticipantsUri(conversationSid));
+        var response = await _client.PostAsync(UrlHelper.AddParticipantsUri(conversationSid), content: content);
+
+        response.EnsureSuccessStatusCode();
     }
 
     public async Task SendMessage(string conversationSid, string body, string author)
     {
         var content = new FormUrlEncodedContent(new Dictionary<string, string> { { "Body", body }, { "Author", author } });
 
-        await _client.PostHttpRequest(content: content, uri: UrlHelper.PostMessageUri(conversationSid));
+        var response = await _client.PostAsync(UrlHelper.PostMessageUri(conversationSid), content: content);
+
+        response.EnsureSuccessStatusCode();
     }
-
-
 }
--- TwilioConversations/Utility/HttpClientExtensions.cs
+++ TwilioConversations/Utility/HttpClientExtensions.cs
@@ -12,21 +12,19 @@ namespace TwilioConversations.Utility;
 
 public static class HttpClientExtensions
 {
-
-    public static async Task<string> PostHttpRequest(this HttpClient client, HttpContent? content = null, string? uri = null)
+    // Might want to create a switch for different verbs instead of ternary
+    public static async Task<T> MakeHttpRequest<T>(this HttpClient client, HttpMethod method, HttpContent? content = null, string? uri = null)
     {
-        var response = await client.PostAsync(uri, content);
+        var response = method == HttpMethod.Get ? await client.GetAsync(uri) : await client.PostAsync(uri, content);
 
         response.EnsureSuccessStatusCode();
 
-        return await response.Content.ReadAsStringAsync();
-    }
-    public static async Task<string> GetHttpRequest(this HttpClient client, HttpContent? content = null, string? uri = null) 
-    {
-        var response = await client.GetAsync(uri);
+        T deserialized;
+        using (var stream = await response.Content.ReadAsStreamAsync())
+        {
+            deserialized = await JsonSerializer.DeserializeAsync<T>(stream) ?? throw new InvalidOperationException("Failed to deserialize response.");
+        }
 
-        response.EnsureSuccessStatusCode();
-
-        return await response.Content.ReadAsStringAsync();
+        return deserialized;
     }
 }
